f888fc1189888230a7c32b20b8cb39069d66ea49
Harviator <78506767+harviator@users.noreply.github.com>
2021-11-05 16:48:31 -0400

    Made changes to App.js file in preparation for for the components and pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
