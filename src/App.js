a4a1872769ee066efd7fe698a943ddd7c5859149
Harviator <78506767+harviator@users.noreply.github.com>
2021-11-05 20:54:29 -0400

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Navbar, Footer } from './components';
 
 function App() {
   return (
