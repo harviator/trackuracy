c5001c7c8c8c4f2b08db759c9b7869e94f46aa0d
Harviator <78506767+harviator@users.noreply.github.com>
2021-11-09 13:43:00 -0500

    Update, getting a failure to complie error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Navbar, Footer } from './components';
+import { Navbar } from './components';
+import Home from './components/pages/Home';
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
       <div>
         <Navbar />
         <Route exact path="/" component={Home} />
-        <Route path="/tier1" component={Teir1} />
-        <Route path="/tier2" component={Teir2} />
+        {/* <Route path="/tier1" component={Tier1} /> */}
+        {/* <Route path="/tier2" component={Tier2} /> */}
         <Footer />
       </div>
     </Router>
