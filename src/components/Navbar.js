44393dd7c09e904b8840beb452b01357476f40fb
Harviator <78506767+harviator@users.noreply.github.com>
2021-11-10 14:30:17 -0500

    Installed react bootstrap, added tackuracy logo, and started working on the navbar from a responsive standpoint

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import trackuracyLogo from "../assets/images/trackuracy-logo.png"
 
-function Navbar() {
+function Navbar(props) {
 
     const location = useLocation();
 
     return (
         <header>
             <nav>
-                <h1>Trackuracy</h1>
+                <img src={trackuracyLogo} alt="Track Accuracy Logo" />
                 <Link to="/" className={location.pathname === "/" ? "active menu" : "menu"}>
                     Home
                 </Link>
