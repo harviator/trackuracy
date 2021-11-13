6572bb0a048b1f5b63d07cfaf1e680870cd8faaf
Harviator <78506767+harviator@users.noreply.github.com>
2021-11-13 17:31:53 -0500

    Added link to JVCKenwood website to JVCKenwood logo in footer. Created a responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
-// import { Container, Row, Col } from 'react-bootstrap';
 import jvckenwood_logo from '../assets/images/jvckenwood_logo.png';
 
 function Footer(props) {
     return (
-        <footer className="justify-content-between">
-            <p className="m-auto">&copy; {new Date().getFullYear()} JVCKenwood Canada Inc.</p>
-            <img id="jvckenwood" src={jvckenwood_logo} alt="JVCKenwood Logo" />
+        <footer>
+            <p className="m-0">&copy; {new Date().getFullYear()} JVCKenwood Canada Inc.</p>
+            <a href="https://ca.jvckenwood.com/" target="_blank" rel="noreferrer">
+                <img id="jvckenwood" src={jvckenwood_logo} alt="JVCKenwood Logo" />
+            </a>
         </footer >
     );
 }
