044bb4259331ae1e92bcf8dec56c348ea4ba41b0
Harviator <78506767+harviator@users.noreply.github.com>
2021-11-16 22:07:21 -0500

    Added some content and styling to home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,26 @@
 import React from "react";
+import rtls from "../../assets/images/rtls.png";
+import Image from 'react-bootstrap/Image'
+import { Container } from "react-bootstrap";
 
 function Home(props) {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h3>FEATURES</h3>
+      <p>With Trackuracy, you can improve safety by tracking assets within your buildings, across all your campus locations, and in between
+buildings. Even while taking to the road in company vehicles,
+your view switches seamlessly between indoor and outdoor
+tracking. Best of all, it happens within a single application.</p>
+      <h6>Real Time Location System (RTLS)</h6>
+      <p>An RTLS helps you get the correct resources to the right place at the right time - all visible to you as it happens.</p>
+      <Container>
+        <Image
+          src={rtls}
+          // height="61"
+          alt="Track Accuracy Logo"
+          fluid
+        />
+      </Container>
     </div>
   );
 }
