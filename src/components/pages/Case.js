55a202703439d474f9e547b75918c8f54b0b3866
Harviator <78506767+harviator@users.noreply.github.com>
2022-01-21 17:14:16 -0500

    Edited CSS to be a truly mobile first responsive web design.  Made some styling changes to the Home Page and navbar.  Next, style the footer with hover states

--- src/components/pages/Case.js
+++ src/components/pages/Case.js
@@ -6,8 +6,9 @@ function Case(props) {
       <h2>Case Studies</h2>
 
       <p>
-        Coming soon!
+        <i>"They've done studies, you know.  60% of the time, it works every time."</i> --Brian Fantana
       </p>
+      <img src="https://static.wikia.nocookie.net/anchorman/images/e/ec/Brian-fantana.jpg" alt="Brian Fantana" />
 
     </div>
   );
