86d930bd7e96ba89bf0f419e447fa3cdbe32aa77
Harviator <78506767+harviator@users.noreply.github.com>
2022-01-26 15:34:01 -0500

    Added hover selectors to the anchor tags in the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,34 +9,25 @@ import YouTube from '../assets/images/youtube.png';
 function Footer(props) {
     return (
         <footer>
-
             <a href="https://ca.jvckenwood.com/" target="_blank" rel="noreferrer">
                 <img id="kenwoodMain" src={kenwoodMain} alt="Kenwood Logo" />
             </a>
-
-
             <a href="https://www.facebook.com/JVCKenwoodCanada" target="_blank" rel="noreferrer">
                 <img id="facebook" src={Facebook} alt="Facebook Logo" />
             </a>
-
             <a href="https://www.instagram.com/kenwoodcanadaofficial/" target="_blank" rel="noreferrer">
                 <img id="instagram" src={Instagram} alt="Instagram Logo" />
             </a>
-
             <a href="https://www.linkedin.com/company/jvckenwood-canada/" target="_blank" rel="noreferrer">
                 <img id="linkedin" src={LinkedIn} alt="LinkedIn Logo" />
             </a>
-
             <a href="https://twitter.com/jvckenwood_can" target="_blank" rel="noreferrer">
                 <img id="twitter" src={Twitter} alt="Twitter Logo" />
             </a>
-
             <a href="https://www.youtube.com/channel/UC8Z5lfCggeK88BDvubvOlPg" target="_blank" rel="noreferrer">
                 <img id="youtube" src={YouTube} alt="YouTube Logo" />
             </a>
-
             <p className="m-0">&copy; {new Date().getFullYear()} JVCKENWOOD Canada Inc.</p>
-
         </footer >
     );
 }
