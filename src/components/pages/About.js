c6eaf22207b5ee0c2656d7e0666d6f8d385db174
Harviator <78506767+harviator@users.noreply.github.com>
2022-03-21 11:26:38 -0400

    Removed the spacing on the About.js page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -16,17 +16,12 @@ import Image from 'react-bootstrap/Image';
 function About(props) {
   return (
     <div className="about-page">
-
       <h2>Why Trackuracy?</h2>
-
       <div className="about-features">
-
         <h3>FEATURES & BENEFITS</h3>
-
         <p>
           Trackuracy integrates indoor and outdoor functionality into simple-to-use tools, making it not just possible, but easy to solve big problems:
         </p>
-
         <ul>
           <li>Receive location information for all emergency calls</li>
           <li>Trace contacts and areas needing sanitization</li>
@@ -41,9 +36,7 @@ function About(props) {
           <li>Visual awareness and integration with the radio emergency button and man-down features</li>
           <li>Allows you to assess response times and resources to drive operations</li>
         </ul>
-
       </div>
-
       <div className="cover-image">
         <Image
           id="cover"
@@ -51,11 +44,8 @@ function About(props) {
           alt={"Woman at a desk using Tackuracy software"}
         />
       </div>
-
       <div className="about-safety">
-
         <h3>NEXT LEVEL WORKPLACE SAFETY & EFFICIENCY AT YOUR FINGERTIPS</h3>
-
         <ul>
           <li>Build situational awareness of regular activity so that developing situations stand out</li>
           <li>Define critical areas and increase alert levels, even within IS and ATEX environments</li>
@@ -67,16 +57,12 @@ function About(props) {
           <li>Automate actions based on location data </li>
         </ul>
       </div>
-
       <div className="about-desktop">
-
         <div className="about-full-text">
           <h3>FEATURES & BENEFITS</h3>
-
           <p>
             Trackuracy integrates indoor and outdoor functionality into simple-to-use tools, making it not just possible, but easy to solve big problems:
           </p>
-
           <ul>
             <li>Receive location information for all emergency calls</li>
             <li>Trace contacts and areas needing sanitization</li>
@@ -91,9 +77,7 @@ function About(props) {
             <li>Visual awareness and integration with the radio emergency button and man-down features</li>
             <li>Allows you to assess response times and resources to drive operations</li>
           </ul>
-
           <h3>NEXT LEVEL WORKPLACE SAFETY & EFFICIENCY AT YOUR FINGERTIPS</h3>
-
           <ul>
             <li>Build situational awareness of regular activity so that developing situations stand out</li>
             <li>Define critical areas and increase alert levels, even within IS and ATEX environments</li>
@@ -105,7 +89,6 @@ function About(props) {
             <li>Automate actions based on location data </li>
           </ul>
         </div>
-
         <Image
           id="collage"
           src={Collage}
